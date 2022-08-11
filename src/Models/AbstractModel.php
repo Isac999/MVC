0e3b4bb7d56acbf45a719e2f7d1cbae946225e23
Isac Fernandes <isac.costa@fatec.sp.gov.br>
2022-08-11 11:06:59 -0300

    changes

--- src/Models/AbstractModel.php
+++ src/Models/AbstractModel.php
@@ -5,9 +5,11 @@ require_once('../Models/Connect.php');
 use models\connect\Connect;
 
 abstract class AbstractModel extends Connect {
+
     public function __construct($database) {
         parent::__construct($database);
     }
+    
     abstract public function insert($data);
     abstract public function update($data);
     abstract public function delete($data);
