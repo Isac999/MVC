c0c39b429e623367c4dd9e882a010d9cfe80001c
Isac Fernandes <isac.costa@fatec.sp.gov.br>
2022-08-11 15:33:26 -0300

    changes

---
+++ src/Controller/Books_rentalsController.php
@@ -0,0 +1,26 @@
+<?php 
+namespace controller;
+
+require_once('../Models/Books_rentals.php');
+require_once('../Utils/switchOperation.php');
+
+use models\crud\Books_rentals;
+use function utils\switchOperation;
+
+class Books_rentalsController {
+
+    public function __construct($json) {
+
+        $list = explode(',"operation":', $json);
+        $operation = str_replace(['}', '"'], '', $list[1]);
+        $json = str_replace(['{', ':', 'id', '"'], '', $list[0]);
+
+        $connect = new Books_rentals('library');
+        
+        switchOperation($operation, $json, $connect);
+    }
+}
+
+$json = file_get_contents('php://input');
+$conn = new Books_rentalsController($json);
+?>
---
+++ src/Models/Books_rentals.php
@@ -0,0 +1,45 @@
+<?php 
+namespace models\crud;
+
+require_once('../Models/AbstractModel.php');
+use models\crud\AbstractModel;
+
+class Books_rentals extends AbstractModel {
+    
+    public function __construct($database) {
+        parent::__construct($database);
+    }
+
+    public function insert($listData) : bool
+    {
+        $exec = "INSERT INTO `books_rentals` (`id`, `book_id`, `customer_id`, `date`) VALUES ('$listData[0]', '$listData[1]', '$listData[2]', '$listData[3]')";
+        $query = $this->mysqli->query($exec) or die('Falha ao executar inserção!');
+        return true;
+    }
+
+    public function update($listData) : bool
+    {
+        $columns = array();
+        $exec_columns = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'library' AND TABLE_NAME = 'books_rentals'";
+        $selectColumns = $this->mysqli->query($exec_columns) or die('Erro ao consultar colunas');
+        
+        while ($column = $selectColumns->fetch_assoc()) {
+            array_push($columns, $column['COLUMN_NAME']);
+        }
+
+        for ($index = 0; $index != count($columns); $index++) {
+            $exec = "UPDATE `books_rentals` SET `$columns[$index]` = '$listData[$index]' WHERE `books_rentals`.`id` = '$listData[0]'";
+            $query = $this->mysqli->query($exec) or die('Falha ao executar update!');
+        }
+        return true;
+    }
+
+    public function delete($json) : bool
+    {
+        $data = $json;
+        $exec = "DELETE FROM `books_rentals` WHERE id = '$data'";
+        $query = $this->mysqli->query($exec) or die("Falha na deleção");
+        return true;
+    }
+}
+?>
