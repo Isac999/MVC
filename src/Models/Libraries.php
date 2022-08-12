9780df3b42c8c9e694fda76cec93376bcd9fef10
Isac Fernandes <isac.costa@fatec.sp.gov.br>
2022-08-12 14:44:13 -0300

    changes

---
+++ src/Models/Libraries.php
@@ -0,0 +1,45 @@
+<?php 
+namespace models\crud;
+
+require_once('../Models/AbstractModel.php');
+use models\crud\AbstractModel;
+
+class Libraries extends AbstractModel {
+    
+    public function __construct($database) {
+        parent::__construct($database);
+    }
+
+    public function insert($listData) : bool
+    {
+        $exec = "INSERT INTO `libraries` (`id`, `localization`) VALUES ('$listData[0]', '$listData[1]')";
+        $query = $this->mysqli->query($exec) or die('Falha ao executar inserção!');
+        return true;
+    }
+
+    public function update($listData) : bool
+    {
+        $columns = array();
+        $exec_columns = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'library' AND TABLE_NAME = 'libraries'";
+        $selectColumns = $this->mysqli->query($exec_columns) or die('Erro ao consultar colunas');
+        
+        while ($column = $selectColumns->fetch_assoc()) {
+            array_push($columns, $column['COLUMN_NAME']);
+        }
+
+        for ($index = 0; $index != count($columns); $index++) {
+            $exec = "UPDATE `libraries` SET `$columns[$index]` = '$listData[$index]' WHERE `libraries`.`id` = '$listData[0]'";
+            $query = $this->mysqli->query($exec) or die('Falha ao executar update!');
+        }
+        return true;
+    }
+
+    public function delete($json) : bool
+    {
+        $data = $json;
+        $exec = "DELETE FROM `libraries` WHERE id = '$data'";
+        $query = $this->mysqli->query($exec) or die("Falha na deleção");
+        return true;
+    }
+}
+?>
